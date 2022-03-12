4b0459af055232bf0508cf6ee045bf430511ab96
hugosenari <hugosenari@gmail.com>
2022-03-12 03:39:12 -0300

    chore: re index tinamou

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Game Library in Nim with SDL2'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."tinamou-master".url = "path:./master";
+  inputs."tinamou-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."tinamou-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
